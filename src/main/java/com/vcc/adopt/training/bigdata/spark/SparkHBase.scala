d6b77f8b9cfec6fdd95865f8449eeae0b7640d33
NamAnhVH <namanh12022002@gmail.com>
2024-02-21 15:01:04 +0700

    debug

--- src/main/java/com/vcc/adopt/training/bigdata/spark/SparkHBase.scala
+++ src/main/java/com/vcc/adopt/training/bigdata/spark/SparkHBase.scala
@@ -7,6 +7,7 @@ import org.apache.hadoop.hbase.TableName
 import org.apache.hadoop.hbase.client.{Get, Put}
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType, TimestampType}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.functions._
@@ -21,7 +22,8 @@ object SparkHBase {
   spark.conf.set("spark.sql.debug.maxToStringFields", 10000)
   private val bai1 = ConfigPropertiesLoader.getYamlConfig.getProperty("bai1")
 
-  private def findKmean(i: Int): Unit = {
+  private def findKmean(k: Int): Unit = {
+    println("Start")
     val data: DataFrame = spark.read.text(bai1)
       .toDF("data")
 
@@ -29,15 +31,26 @@ object SparkHBase {
     val parsedData = data.selectExpr("split(data, ',')[0] as x", "split(data, ',')[1] as y")
 
     // Hiển thị dữ liệu
-    parsedData.show()
+    parsedData.show(-1)
+
+    // Tạo một đối tượng KMeans
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("x", "y"))
+      .setOutputCol("features")
+
+    // Áp dụng chuyển đổi vào dữ liệu
+    val dataWithFeatures = assembler.transform(parsedData)
+
+    // Hiển thị dữ liệu đã chuyển đổi
+    dataWithFeatures.show(-1)
 
     // Tạo một đối tượng KMeans
     val kmeans = new KMeans()
-      .setK(3) // Số lượng cụm
+      .setK(k) // Số lượng cụm
       .setSeed(1L) // Seed để tái tạo kết quả
 
     // Tạo và fit mô hình KMeans
-    val model = kmeans.fit(parsedData)
+    val model = kmeans.fit(dataWithFeatures)
 
     // Tính toán các centroid cuối cùng
     val centroids = model.clusterCenters
@@ -46,7 +59,7 @@ object SparkHBase {
     centroids.foreach(println)
 
     // Dự đoán cụm cho mỗi điểm dữ liệu
-    val predictions = model.transform(parsedData)
+    val predictions = model.transform(dataWithFeatures)
 
     // Hiển thị kết quả
     predictions.show()
