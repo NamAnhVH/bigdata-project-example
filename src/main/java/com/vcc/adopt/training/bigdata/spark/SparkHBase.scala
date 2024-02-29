c80a6779de333c97e8d91b19e906f4b36924359d
NamAnhVH <namanh12022002@gmail.com>
2024-02-29 14:20:28 +0700

    5.1

--- src/main/java/com/vcc/adopt/training/bigdata/spark/SparkHBase.scala
+++ src/main/java/com/vcc/adopt/training/bigdata/spark/SparkHBase.scala
@@ -355,7 +355,7 @@ object SparkHBase {
         val table = hbaseConnection.getTable(TableName.valueOf("bai5", "dept_emp"))
         try {
           rows.map(row => {
-            val get = new Get(Bytes.toBytes(deptNo + "_" + row.getAs[Int]("emp_no")))
+            val get = new Get(Bytes.toBytes(deptNo + "_" + row.getAs[String]("emp_no")))
             get.addColumn(Bytes.toBytes("employee"), Bytes.toBytes("emp_no"))
             get.addColumn(Bytes.toBytes("employee"), Bytes.toBytes("birth_date"))
             get.addColumn(Bytes.toBytes("employee"), Bytes.toBytes("first_name"))
@@ -387,7 +387,7 @@ object SparkHBase {
         val table = hbaseConnection.getTable(TableName.valueOf("bai5", "dept_emp"))
         try {
           rows.map(row => {
-            val get = new Get(Bytes.toBytes(deptNo + "_" + row.getAs[Int]("emp_no")))
+            val get = new Get(Bytes.toBytes(deptNo + "_" + row.getAs[String]("emp_no")))
             get.addColumn(Bytes.toBytes("manager"), Bytes.toBytes("dm_from_date"))
             get.addColumn(Bytes.toBytes("employee"), Bytes.toBytes("emp_no"))
             get.addColumn(Bytes.toBytes("employee"), Bytes.toBytes("birth_date"))
