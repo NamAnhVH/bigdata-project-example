479e69428f25d8425f5320624dbb517a8fe8ad7d
NamAnhVH <namanh12022002@gmail.com>
2024-02-29 16:30:28 +0700

    debug

--- src/main/java/com/vcc/adopt/training/bigdata/spark/SparkHBase.scala
+++ src/main/java/com/vcc/adopt/training/bigdata/spark/SparkHBase.scala
@@ -485,12 +485,73 @@ object SparkHBase {
 
   }
 
+  private def readHbase53(empNo: Int): Unit = {
+    println("----- Lấy chức vụ của một nhân viên cần truy vấn ----")
+
+    var row_key : DataFrame = null
+
+    try {
+      // Load driver
+      Class.forName("com.mysql.cj.jdbc.Driver")
+
+      // Tạo kết nối
+      connection = DriverManager.getConnection(url, username, password)
+
+      // Thực hiện truy vấn
+      val statement = connection.createStatement()
+      val query = "SELECT distinct from_date FROM titles"
+      resultSet = statement.executeQuery(query)
+
+      row_key = {
+        import spark.implicits._
+        val rows = Iterator.continually(resultSet).takeWhile(_.next()).map { row =>
+          (row.getString("row_key"))
+        }
+        val df = rows.toSeq.toDF("row_key")
+        df
+      }
+
+    } catch {
+      case e: Exception => e.printStackTrace()
+    } finally {
+      // Đóng kết nối
+      if (resultSet != null) resultSet.close()
+      if (connection != null) connection.close()
+    }
+    import spark.implicits._
+    val titleDF = row_key
+      .repartition(5)
+      .mapPartitions((rows: Iterator[Row]) => {
+        val hbaseConnection = HBaseConnectionFactory.createConnection()
+        val table = hbaseConnection.getTable(TableName.valueOf("bai5", "titles"))
+        try {
+          rows.flatMap(row => {
+            val get = new Get(Bytes.toBytes(empNo + "_" + row.getAs[String]("row_key")))
+            get.addColumn(Bytes.toBytes("info"), Bytes.toBytes("title"))
+            if (table.get(get).getValue(Bytes.toBytes("info"), Bytes.toBytes("title")) != null) {
+              Some(
+                Bytes.toString(table.get(get).getValue(Bytes.toBytes("info"), Bytes.toBytes("title")))
+              )
+            }
+            else {
+              None
+            }
+          })
+        }finally {
+          //          hbaseConnection.close()
+        }
+      }).toDF("title")
+
+    titleDF.show()
+  }
+
 
   def main(args: Array[String]): Unit = {
 //    readMySqlThenPutToHBaseDeptEmp()
 //    readMySqlThenPutToHBaseSalaries()
 //    readMySqlThenPutToHBaseTitles()
 //    readHbase51("d001")
-    readHbase52("1986-08-26")
+//    readHbase52("1986-08-26")
+    readHbase53(100001)
   }
 }
